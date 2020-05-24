9b62766796de7c2205c9943d09eea3616eb565a0
star_dust_evolution <429736040@qq.com>
2020-05-24 18:34:42 +0800

    new

---
+++ control/js/common.js
@@ -0,0 +1,22 @@
+
+/*
+    note：总控
+    anthor：zx
+*/
+
+var step = $("#step");
+var commonlink = 'http://server3.foshanplus.com/';
+
+$(document).ready(function() {
+    initMainview()
+    initSize();
+    initFile();
+    initLiteswitchp3();
+    initLitestep();
+
+    getoldprjs();
+});
+
+window.onload = function() {
+    
+}
